04d4c1ad19339fc4cdf9cb43415e748054aa9955
mtahmazli <mrdtehmezli@gmail.com>
2024-12-08 17:21:44 +0400

    updateLogin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
  
 
 
-  Restrict admin access
+  
   def authenticate_admin!
     redirect_to root_path, alert: 'Not authorized' unless current_user&.admin?
   end
